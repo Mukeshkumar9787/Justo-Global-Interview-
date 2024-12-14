07a99ab8aa0ae02948545bca084b64b8d329f440
Mukeshkumar9787 <logeshsmart72@gmail.com>
2024-12-14 08:55:34 +0530

    api

--- src/db/insert.sql
+++ src/db/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO `justoglobal`.`user` (`id`, `email`, `mobile`, `password`) VALUES ('1', 'mukeshkumar8708@gmail.com', '9787291234', 'Mukesh');
+INSERT INTO `user` (`id`, `email`, `mobile`, `password`) VALUES ('1', 'mukeshkumar8708@gmail.com', '9787291234', 'Mukesh');
